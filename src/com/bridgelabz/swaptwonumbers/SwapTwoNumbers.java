24779c8b62c351cc0457a8ff8c56268ebb9b5f07
Rohit <rohitkamble10792@gmail.com>
2022-05-22 16:35:49 +0530

    Swaping of two numbers is done

--- src/com/bridgelabz/swaptwonumbers/SwapTwoNumbers.java
+++ src/com/bridgelabz/swaptwonumbers/SwapTwoNumbers.java
@@ -1,7 +1,24 @@
 package com.bridgelabz.swaptwonumbers;
 
+import java.util.*;
+
 public class SwapTwoNumbers {
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Swaping of two numbers");
+        int x, y, t;// x and y are to swap
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of X and Y");
+        x = sc.nextInt();
+        y = sc.nextInt();
+        System.out.println("before swapping numbers: "+x +"  "+ y);
+        /*swapping */
+        t = x;
+        x = y;
+        y = t;
+        System.out.println("After swapping: "+x +"   " + y);
+        System.out.println( );
     }
 }
+
+
+
