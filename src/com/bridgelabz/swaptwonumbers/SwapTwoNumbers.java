e95c761b06242d68a09930f0a748c6477726b587
Rohit <rohitkamble10792@gmail.com>
2022-05-22 16:31:33 +0530

    Welcome to Swaping of two number program

---
+++ src/com/bridgelabz/swaptwonumbers/SwapTwoNumbers.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.swaptwonumbers;
+
+public class SwapTwoNumbers {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Swaping of two numbers");
+    }
+}
